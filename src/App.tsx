2a80dd9dfddce350cc9bbf284ea11c9081577071
n <nicholas@zainawe.com>
2021-07-27 16:59:08 +1000

    default props

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,37 @@
 import React, { ReactElement, ReactNode } from 'react';
 import './App.css';
 
-
-
 // Conventional Props
 function Heading({ title }: { title: string }) {
   return <h1>{title}</h1>
 }
-function HeadingWithContent({ children }: { children:  ReactNode }): ReactElement {
+function HeadingWithContent({ children }: { children: ReactNode }): ReactElement {
   return <h1>{children}</h1>
 }
+
+// default Props
+const defaultContainerProps = {
+  heading: <strong>My Heading</strong>
+}
+type ContainerProps = { children: ReactNode } & typeof defaultContainerProps;
+function Container({
+  heading,
+  children,
+}: ContainerProps): ReactElement {
+  return <div><h1>{heading}</h1>{children}</div>
+}
+
+Container.defaultProps =  defaultContainerProps;
 
 function App() {
   return (
     <div>
       <Heading title="Hello"></Heading>
       <HeadingWithContent>
-       <strong>Hi</strong>
-        </HeadingWithContent>
+        <strong>Hi</strong>
+      </HeadingWithContent>
+      <Container>Foo</Container>
+     
     </div>
   );
 }
